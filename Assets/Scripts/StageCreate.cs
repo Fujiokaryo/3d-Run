210e6c28d0939fc245af4bb028496d5dad2b422a
Fujiokaryo <f.tooru17@gmail.com>
2021-01-14 22:45:05 +0900

    追従カメラ追加　ステージのループ生成追加

---
+++ Assets/Scripts/StageCreate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Stage1;
+
+    [SerializeField]
+    public GameObject Stage2;
+
+    int border = 1000;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > border)
+        {
+            CreateMap();
+        }
+    }
+
+    void CreateMap()
+    {
+        if(Stage1.transform.position.z < border)
+        {
+            border += 2000;
+
+            Vector3 temp = new Vector3(0, 0.05f, border);
+
+            Stage1.transform.position = temp;
+        }
+        else if(Stage2.transform.position.z < border)
+        {
+            border += 2000;
+
+            Vector3 temp = new Vector3(0, 0.05f, border);
+
+            Stage2.transform.position = temp;
+        }
+    }
+}
