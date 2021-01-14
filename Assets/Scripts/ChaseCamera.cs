c23c3c513c5cdcbcd5b1917cc3c6066f14bdf0cc
Fujiokaryo <f.tooru17@gmail.com>
2021-01-14 22:14:19 +0900

    仮プレイヤーの自走、レール移動の実装　HPゲージ、障害物に当たった際のHP減処理の実装

---
+++ Assets/Scripts/ChaseCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    private Vector3 offset;
+    void Start()
+    {
+        //カメラとプレイヤーの位置関係の取得
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            transform.position = target.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Image HPimg;
+
+    [SerializeField]
+    float speed;
+
+    private float PlayerHP = 1;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Accel();
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Right(); //右移動
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Left(); //左移動
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Wall")
+        {
+            PlayerHP -= 0.25f;
+            HPimg.DOFillAmount(PlayerHP, 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// 自走用メソッド
+    /// </summary>
+    void Accel()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+    }
+
+    /// <summary>
+    /// レーン移動(右）
+    /// </summary>
+    void Right()
+    {
+
+        if (transform.position.x == -5)
+        {
+            transform.Translate(3.5f, 0, 0);
+        }
+        else if (transform.position.x == -1.5)
+        {
+            transform.Translate(3f, 0, 0);
+        }
+        else if (transform.position.x == 1.5)
+        {
+            transform.Translate(3.5f, 0, 0);
+        }
+
+    }
+
+    /// <summary>
+    /// レーン移動（左）
+    /// </summary>
+    void Left()
+    {
+        if (transform.position.x == 5)
+        {
+            transform.Translate(-3.5f, 0, 0);
+        }
+        else if (transform.position.x == 1.5)
+        {
+            transform.Translate(-3f, 0, 0);
+        }
+        else if (transform.position.x == -1.5)
+        {
+            transform.Translate(-3.5f, 0, 0);
+        }
+        
+    }
+ 
+}
